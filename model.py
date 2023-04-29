e1fb5239291befc68292c5310559547672f1000a
Carsten Birck Jensen <g@bircks.dk>
2023-04-29 11:42:06 +0200

    Add TodoList class

--- model.py
+++ model.py
@@ -5,3 +5,19 @@ class TodoItem:
 
     def __str__(self):
         return f"{self.description} ({'complete' if self.completed else 'incomplete'})"
+
+class TodoList:
+    def __init__(self):
+        self.items = []
+
+    def add_item(self, description):
+        self.items.append(TodoItem(description))
+
+    def remove_item(self, index):
+        del self.items[index]
+
+    def mark_complete(self, index):
+        self.items[index].completed = True
+
+    def mark_incomplete(self, index):
+        self.items[index].completed = False
